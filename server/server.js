973361162960360b1261e9381cb821b8d6ffa4ae
Leon <leon.karho.chung@gmail.com>
2017-10-06 16:26:10 -0400

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -147,6 +147,14 @@ app.post('/users/login', function(req, res) {
     })
 });
 
+app.delete('/users/me/token', authenticate, function(req, res) {
+    req.user.removeToken(req.token).then(function() {
+        res.status(200).send();
+    }, function() {
+        res.status(400).send();
+    });
+});
+
 // ***SERVER***
 app.listen(port, function() {
     console.log(`Started up at port ${port}`);
